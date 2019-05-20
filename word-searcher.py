d4c0417efdd24cdc4612498da85ea0200b1f15e6
Emil Brännström <emil.brannstrom@ibm.com>
2019-05-20 08:53:53 +0200

    adding dictionary to loop for matching keyword counts to textfiles

--- word-searcher.py
+++ word-searcher.py
@@ -1,9 +1,13 @@
-#import OS module to enable underlying OS capabilities
-import os
+import os #OS module enables underlying OS capabilities
+import heapq #Heapq module enables heap data structure to find n largest values in a dataset
 
 directory = '/Users/emilbrannstrom/Documents/dev/word-searcher/textfiles'
 
+#create dictionary with textfiles as key and their associated keyword count as value
+fileDict = {} 
+
 matchingWords = []
+
 numberOfMatches = len(matchingWords)
 
 #list all files in directory
@@ -17,6 +21,8 @@ if len(keywords) == 0:
     print('No keyword given')
 print('Your keywords: ' + str(keywords))
 
+
+
 #iterate through all textfiles in directory
 for file in allFiles:
     path = (directory + '/' + file)
@@ -26,16 +32,21 @@ for file in allFiles:
                 for word in line.split():
                     if word in keywords:
                         matchingWords.append(word)
+                        fileDict[file] = len(matchingWords)
             print('Matches found in ' + file + ' :' + str(matchingWords))
 
+print fileDict
+topTen = heapq.nlargest(10, fileDict, len(matchingWords()))
+print("Top 10 files matching your keywords:") 
+print topTen
+"""
 numberOfKeywords = len(keywords)
 
 matchPercentage = numberOfMatches/float(numberOfKeywords)
 
-#print('Number of keywords: ' + str(numberOfKeywords))
 print('Number of matches: ' + str(numberOfMatches))
 if len(matchingWords) < 1:
     print('Matches found: none')
 else: 
     print('Matches found: ' + str(matchingWords))
-print('Percentage of your words that were found: ' + str(matchPercentage*100))
+print('Percentage of your words that were found: ' + str(matchPercentage*100))"""
