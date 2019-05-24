fda43aeb60e852a72e977196c1da4b1d6857ea2f
Emil Brännström <emil.brannstrom@ibm.com>
2019-05-24 13:04:31 +0200

    Fixed correct match ratio when finding multiple word matches.

--- word-searcher.py
+++ word-searcher.py
@@ -7,28 +7,31 @@ directory = '/Users/emilbrannstrom/Documents/dev/word-searcher/textfiles'
 allFiles = os.listdir('textfiles/')
 
 #prompt for keywords
-keywords = input('Enter words to search for: ').split()
+keywords = input('Enter words to search for: ').lower().split()
 
 nrOfKeywords = len(keywords)
 
-count = 0
+keywordsInText = []
 
 #create empty dictionary for pairing textfiles with number of keyword matches
+
 results = {} 
 
 #iterate through all textfiles in directory and search for keyword matches
 for file in allFiles:
+    #fileList = []
     path = (directory + '/' + file)
     if file.endswith('.txt'):
-        with open(path, 'r') as openFile: #open and automatically close file
-            text = openFile.read().split() #read text and split into single words
+        with open(path, 'rt') as openFile: #open and automatically close file
+            text = openFile.read().lower().split() #read text and split into single words
             uniqueWords = list(dict.fromkeys(text))
             for word in keywords: #for each keyword, do the following:
                 if word in uniqueWords: #check if keyword matches any word in text
-                    count += 1
-                    results[file] = count #add number of matches to corresponding file in dictionary
-                    
-print('number of keywords: \n',nrOfKeywords)
+                    keywordsInText.append(word)
+                    print(keywordsInText)
+                    results[file] = len(keywordsInText) #add number of matches to corresponding file in dictionary
+            keywordsInText = []
+print('number of keywords: \n',nrOfKeywords) 
 
 for key, value in results.items():
     results[key] = float(value)/float(nrOfKeywords) * 100
@@ -38,8 +41,7 @@ print('Matches found in: \n',results)
 
 """
 TODO: 
-- Improve match ratio for each textfile.
+- Improve match ratio for each textfile. done
 - Limit result to only show top 10.
-- Fix upper limit on counter.
-- Transform to lowercase.
+- Transform to lowercase. done
 """
