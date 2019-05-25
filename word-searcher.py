5ca34b7f0f532aae7faadc916dae15a6f1f62513
Emil Brännström <emil.brannstrom@ibm.com>
2019-05-25 09:48:17 +0200

    Fixing output to be displayed in descending match ratio order.

--- word-searcher.py
+++ word-searcher.py
@@ -1,5 +1,5 @@
 import os #Enable underlying OS capabilities
-import operator #Enable a set of standard operators as functions
+from heapq import nlargest #Enable extraction of n greatest elements in dictionary
 
 #path to directory to scan for files
 directory = '/Users/emilbrannstrom/Documents/dev/word-searcher/textfiles'
@@ -7,14 +7,14 @@ directory = '/Users/emilbrannstrom/Documents/dev/word-searcher/textfiles'
 #create list of all files in directory
 allFiles = os.listdir('textfiles/')
 
-#prompt for keywords
+#Prompt for keywords
 keywords = input('Enter words to search for: ').lower().split()
 
 nrOfKeywords = len(keywords)
 
 keywordsInText = []
 
-#create empty dictionary for pairing textfiles with number of keyword matches
+#Create empty dictionary for pairing textfiles with number of keyword matches
 
 results = {} 
 
@@ -23,23 +23,27 @@ for file in allFiles:
     #fileList = []
     path = (directory + '/' + file)
     if file.endswith('.txt'):
-        with open(path, 'rt') as openFile: #open and automatically close file
-            text = openFile.read().lower().split() #read text and split into single words
+        with open(path, 'rt') as openFile: #Open and automatically close file
+            text = openFile.read().lower().split() #Read text and split into single words
             uniqueWords = list(dict.fromkeys(text))
-            for word in keywords: #for each keyword, do the following:
-                if word in uniqueWords: #check if keyword matches any word in text
+            for word in keywords: #For each keyword, do the following:
+                if word in uniqueWords: #Check if keyword matches any word in text
                     keywordsInText.append(word)
-                    results[file] = len(keywordsInText) #add number of matches to corresponding file in dictionary
+                    results[file] = len(keywordsInText) #Add number of matches to corresponding file in dictionary
             keywordsInText = []
 
 for key, value in results.items():
     results[key] = float(value)/float(nrOfKeywords) * 100
 
-#show top 10 matched files and corresponding match ratio
-for key, value in results.items():
+#Transform results into a sorted list, and then back into a dictionary
+sortedResults = dict(sorted(results.items(), key=lambda x: x[1], reverse=True)) 
+
+#Show matched files and corresponding match ratio
+for key, value in sortedResults.items():
     print(int(value),'% match in ',key)
 
 """
 TODO: 
-- Make list of result prettier.
+- Limit output to show only top 10 results.
+- Show hits with 0 % in output.
 """
